9845d613e7a557a62ab8ee830a65544eaee6ddad
Nuzhat <nuzu23.t@gmail.com>
2022-01-05 15:19:48 -0700

    corrected simplified form and added new test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,6 @@ form(1,0,0,1,1,0,0,1,0.66)
 @testset "FormFactor.jl" begin
 
 
-    @test  isapprox(form(1,0,0,1,1,0,0,1,0.66),0.813233448)# Write your tests here.
+    @test  isapprox(form(1,0,0,1,1,0,0,1,0.66),0.813233;atol=.000001)
+    @test  isapprox(form(2,0,0,1,2,0,0,.5,0.66),0.09305;atol=.000001)# Write your tests here.
 end
