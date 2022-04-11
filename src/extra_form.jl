5ff0ae740f710c4751815652108909d4d72fb962
Nuzhat <nuzu23.t@gmail.com>
2022-04-11 15:19:17 -0600

    introduce rho

--- src/extra_form.jl
+++ src/extra_form.jl
@@ -1,20 +1,20 @@
-function mform(n1::Int64,l1::Int64,m1::Int64,a1::Float64,n2::Int64,l2::Int64,m2::Int64,a2::Float64,q::Float64)
+function mform(n1::Int64,l1::Int64,m1::Int64,n2::Int64,l2::Int64,ρ::Float64,q::Float64)
 
     # The function wigner3j_f produces a vector of wigner symbols where each index corresponds to a value of l between
     # abs(l1-l2) and l1+l2 (and assumes m = -m1-m2) meaning that:
     #    collect(wigner3j_f(l1, l2, m1, m2)) ≈ [wigner3j(l1, l2, l3, m1, m2, -m1-m2) for l ∈ abs(l1-l2):l1+l2]
 
     wigner3j_l1_l2__0_0 = wigner3j_f(l1, l2, 0, 0)
-    wigner3j_l1_l2__m1_negm2 = wigner3j_f(l1, l2, m1, -m2)
+    wigner3j_l1_l2__m1_negm2 = wigner3j_f(l1, l2, m1, -m1)
 
-    δ = (a2*n2/a1+a1*n1)/(n1*a2*n2)
-    β = 2/(a1*n1)
+    δ = (ρ*n2+ n1)/(n1*ρ*n2)
+    β = 2/n1
     α = 2*l1+1
     dd = -q^2/δ^2
-    N1 = (4π*4/(a2^(3/2) * (n1 * n2)^2)
+    N1 = (4π*4/(ρ^(3/2) * (n1 * n2)^2)
           * √(gamma(n1-l1) * gamma(n2-l2)
               / (gamma(n1+l1+1) * gamma(n2+l2+1)))
-          * (2/(a1 * n1))^l1 * (2/(a2 * n2/a1))^l2 * √(π/(2q)))
+          * (2/(n1))^l1 * (2/(ρ * n2))^l2 * √(π/(2q)))
 
     if n1 > l1 >= m1 && n2 > l2 >= m1
         # @floop from the package Floops.jl is able to parallelize a loop for us. If the loop contains a 'reduction'
@@ -25,10 +25,10 @@ function mform(n1::Int64,l1::Int64,m1::Int64,a1::Float64,n2::Int64,l2::Int64,m2:
             for k in 0:n2-l2-1
                 for k1 in 0:n1-l1-1
                     if (l1+l2) % 2 == 0
-                        @reduce Sum += mfun(n1,l1,m1,a1,n2,l2,m2,a2,q,k,k1,l, δ, β, α, dd, N1,
-                                            wigner3j_l1_l2__0_0, wigner3j_l1_l2__m1_negm2) 
+                        @reduce Sum += mfun(n1,l1,m1,n2,l2,ρ,q,k,k1,l, δ, β, α, dd, N1,
+                                            wigner3j_l1_l2__0_0, wigner3j_l1_l2__m1_negm2)
                     else
-                        @reduce Sum += im * mfun(n1,l1,m1,a1,n2,l2,m2,a2,q,k,k1,l,  δ, β, α, dd, N1,
+                        @reduce Sum += im * mfun(n1,l1,m1,n2,l2,ρ,q,k,k1,l,  δ, β, α, dd, N1,
                                                  wigner3j_l1_l2__0_0, wigner3j_l1_l2__m1_negm2)
                     end
                 end
@@ -40,7 +40,7 @@ function mform(n1::Int64,l1::Int64,m1::Int64,a1::Float64,n2::Int64,l2::Int64,m2:
     end
 end
 
-function mfun(n1::Int64,l1::Int64,m1::Int64,a1::Float64,n2::Int64,l2::Int64,m2::Int64,a2::Float64,q::Float64,k::Int64,k1::Int64,l::Int64,
+function mfun(n1::Int64,l1::Int64,m1::Int64,n2::Int64,l2::Int64,ρ::Float64,q::Float64,k::Int64,k1::Int64,l::Int64,
               δ, β, α, dd, N1,
               wigner3j_l1_l2__0_0, wigner3j_l1_l2__m1_negm2)
     m=0
@@ -52,7 +52,7 @@ function mfun(n1::Int64,l1::Int64,m1::Int64,a1::Float64,n2::Int64,l2::Int64,m2::
     cc = 1+ν
 
     I1 = (gamma(n2+l2+1) * (-2)^k * (-β)^k1 * q^ν * gamma(n+α+1)*gamma(ν+γ+k1+1)
-          / (gamma(k+1) * gamma(n2-l2-k) * gamma(2*l2+k+2) * (a2*n2/a1)^k * gamma(k1+1)
+          / (gamma(k+1) * gamma(n2-l2-k) * gamma(2*l2+k+2) * (ρ*n2)^k * gamma(k1+1)
              * gamma(n-k1+1) * gamma(α+k1+1) * 2^ν * gamma(ν+1) * δ^(ν+γ+k1+1)) * _₂F₁(aa,bb,cc,dd))
 
     if (l1+l2) % 2 == 0
--- test/my_form_test.jl
+++ test/my_form_test.jl
@@ -2,13 +2,13 @@ using FormFactor
 using Test
 
 @testset "FormFactor.jl" begin
-    @test  isapprox(mform(1,0,0,1.0,1,0,0,1.0,0.66),0.813233 ;atol=.000001)
-    @test  isapprox(mform(2,0,0,1.0,2,0,0,2.0,0.66),-0.0168244 ;atol=.000001)
-    @test  isapprox(mform(3,1,0,1.0,2,0,0,1.0,0.66),-0.136364im ;atol=.000001)
-    @test  isapprox(mform(3,0,0,1.0,3,0,0,1.0,0.66),0.0433238 ;atol=.000001)
-    @test  isapprox(mform(3,0,0,1.0,3,0,0,2.0,0.66),-0.00422855 ;atol=.000001)
-    @test  isapprox(mform(4,3,2,1.0,4,3,2,2.0,0.66),-0.00334226 ;atol=.000001)
-    @test  isapprox(mform(16,0,0,1.0,16,0,0,2.0,0.66),0.0000861344 ;atol=.000001)
-    @test  isapprox(mform(16,0,0,1.0,16,0,0,1.0,0.66),0.0000898579 ;atol=.000001)
-    @test  isapprox(mform(19,18,18,1.0,19,18,18,1.0,0.66),7.7825e-33 ;atol=.000001)
+    @test  isapprox(mform(1,0,0,1,0,1.0,0.66),0.813233 ;atol=.000001)
+    @test  isapprox(mform(2,0,0,2,0,2.0,0.66),-0.0168244 ;atol=.000001)
+    @test  isapprox(mform(3,1,0,2,0,1.0,0.66),-0.136364im ;atol=.000001)
+    @test  isapprox(mform(3,0,0,3,0,1.0,0.66),0.0433238 ;atol=.000001)
+    @test  isapprox(mform(3,0,0,3,0,2.0,0.66),-0.00422855 ;atol=.000001)
+    @test  isapprox(mform(4,3,2,4,3,2.0,0.66),-0.00334226 ;atol=.000001)
+    @test  isapprox(mform(16,0,0,16,0,2.0,0.66),0.0000861344 ;atol=.000001)
+    @test  isapprox(mform(16,0,0,16,0,1.0,0.66),0.0000898579 ;atol=.000001)
+    @test  isapprox(mform(19,18,18,19,18,1.0,0.66),7.7825e-33 ;atol=.000001)
 end
