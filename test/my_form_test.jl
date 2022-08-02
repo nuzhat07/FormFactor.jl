87d2bcab56681beeda29caf990b9fba02240fa88
Nuzhat <nuzu23.t@gmail.com>
2022-08-02 11:01:34 -0600

    removed imaginary part from the tests.

--- test/my_form_test.jl
+++ test/my_form_test.jl
@@ -4,7 +4,7 @@ using Test
 @testset "FormFactor.jl" begin
     @test  isapprox(mform(1,0,0,1,0,1.0,0.66),0.813233 ;atol=.000001)
     @test  isapprox(mform(2,0,0,2,0,2.0,0.66),-0.0168244 ;atol=.000001)
-    @test  isapprox(mform(3,1,0,2,0,1.0,0.66),-0.136364im ;atol=.000001)
+    @test  isapprox(mform(3,1,0,2,0,1.0,0.66),-0.136364 ;atol=.000001)
     @test  isapprox(mform(3,0,0,3,0,1.0,0.66),0.0433238 ;atol=.000001)
     @test  isapprox(mform(3,0,0,3,0,2.0,0.66),-0.00422855 ;atol=.000001)
     @test  isapprox(mform(4,3,2,4,3,2.0,0.66),-0.00334226 ;atol=.000001)
